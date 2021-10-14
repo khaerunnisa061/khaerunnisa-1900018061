83278eaf19369365c6410960c0f73662819b6506
khaerunnisa061 <63095714+khaerunnisa061@users.noreply.github.com>
2021-10-14 19:26:49 +0700

    pertemuan2
    pertemuan 2

---
+++ akademik2.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 14 Okt 2021 pada 14.22
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `akademik2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `NIM` varchar(50) NOT NULL,
+  `Nama` varchar(255) NOT NULL,
+  `J_kel` varchar(255) NOT NULL,
+  `Alamat` text DEFAULT NULL,
+  `tgl_lahir` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`NIM`, `Nama`, `J_kel`, `Alamat`, `tgl_lahir`) VALUES
+('MHS01', 'Siti Aminah', 'P', 'SOLO', '1995-10-01'),
+('MHS02', 'Rita', 'P', 'SOLO', '1999-01-01'),
+('MHS03', 'Amirudin', 'L', 'SEMARANG', '1998-08-11'),
+('MHS04', 'Siti Maryam', 'P', 'JAKARTA', '1995-04-15');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`NIM`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
